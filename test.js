fd971d53a1d74ba5ed00390709ab3c167b437035
annatorok <mail@annatorok.com>
2016-07-25 20:31:00 +0200

    Fix for tests

--- test.js
+++ test.js
@@ -4,22 +4,21 @@ var tape = require('tape');
 var decrypt = require('./decrypt');
 
 tape('test with positiv shift with all data', function (t) {
-  t.deepEqual(decrypt.decode('oruhp lsvxp groru vlw', 3), {
-    status: 'ok', text: 'lorem ipsum dolor sit' });
+  t.deepEqual(decrypt.decode('oruhp lsvxp groru vlw', 3), 'lorem ipsum dolor sit' );
   t.end();
 });
 
 tape('test with negativ shift with all data', function (t) {
-  t.deepEqual(decrypt.decode('oruhp lsvxp groru vlw', 3), {status: 'ok', text: 'lorem ipsum dolor sit'});
+  t.deepEqual(decrypt.decode('oruhp lsvxp groru vlw', 3), 'lorem ipsum dolor sit');
   t.end();
 });
 
 tape('test with no shift with all data', function (t) {
-  t.deepEqual(decrypt.decode('lorem ipsum dolor sit', 0), {status: 'ok', text: 'lorem ipsum dolor sit'});
+  t.deepEqual(decrypt.decode('lorem ipsum dolor sit', 0), 'lorem ipsum dolor sit');
   t.end();
 });
 
 tape('test with positiv shift with special characters', function (t) {
-  t.deepEqual(decrypt.decode('*)', 2), {status: 'ok', text: '*)'});
+  t.deepEqual(decrypt.decode('*)', 2), '*)');
   t.end();
 });
